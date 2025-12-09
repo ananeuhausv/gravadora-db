5033d45c456dbea2beb4dae775c30dc5cf6dde46
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-12-08 22:28:40 -0300

    consulta 9

---
+++ bd/query09.sql
@@ -0,0 +1,20 @@
+-- Consulta 09: Lista o nome dos produtores musicais que trabalharam em mais de 3 músicas diferentes. Exibe o nome do produtor e a quantidade de músicas produzidas.
+
+SELECT
+    P.nome,
+    COUNT(MP.id_musica) AS musicas_produzidas
+
+FROM
+    estudio.produtor PROD
+
+INNER JOIN
+    estudio.pessoa P ON PROD.id_pessoa = P.id_pessoa
+
+INNER JOIN
+    estudio.musica_produtor MP ON PROD.id_produtor = MP.id_produtor
+
+GROUP BY
+    P.id_pessoa, P.nome
+
+HAVING
+    COUNT(MP.id_musica) > 5;
