1becaa625e2a48e95e59cc0fa35883ca9dd6171c
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-12-08 22:28:10 -0300

    consulta 4

---
+++ bd/query04.sql
@@ -0,0 +1,22 @@
+-- Consulta 04: Lista o nome artístico, o nome do prêmio e a quantidade de vezes que cada artista recebeu cada prêmio. Ordenado pelo nome do artista e pela quantidade em ordem decrescente.
+
+
+SELECT 
+    A.nome_artistico,
+    P.nome,
+    COUNT(*) AS quantidade
+
+FROM 
+    estudio.artista A
+
+INNER JOIN 
+    estudio.artista_premio AP ON A.id_pessoa = AP.id_artista
+
+JOIN 
+    estudio.premio P ON P.id_premio = AP.id_premio
+
+GROUP BY 
+    A.nome_artistico, P.nome
+    
+ORDER BY 
+    A.nome_artistico, quantidade DESC;
